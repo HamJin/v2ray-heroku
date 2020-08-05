edd593bda0d6efa3a76694fd39e83bf4d85398b4
railjty <335908093@qq.com>
2020-08-05 11:57:25 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+mkdir -m 777 /v2raybin
+cd /v2raybin
 curl -L -H "Cache-Control: no-cache" -o v2ray.zip https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip \
 unzip v2ray.zip 
 unzip v2ray.zip 
@@ -5,14 +7,6 @@ chmod +x /v2raybin/v2ray
 rm -rf v2ray.zip \
 chgrp -R 0 /v2raybin 
 chmod -R g+rwX /v2raybin 
-
-#echo -e -n "$CONFIG_JSON1" > config.json
-#echo -e -n "$PORT" >> config.json
-#echo -e -n "$CONFIG_JSON2" >> config.json
-#echo -e -n "$UUID" >> config.json
-#echo -e -n "$CONFIG_JSON3" >> config.json
-
-sed -i "s/66666/$PORT/g" config.json
-#sed -i "s/your_uuid/$UUID/g" config.json
-
+curl -L -H "Cache-Control: no-cache" https://raw.githubusercontent.com/railjty/v2hero/master/config1.json > ./config1.json ;
+sed "s/65537/$PORT/" config1.json > config.json ;
 ./v2ray
