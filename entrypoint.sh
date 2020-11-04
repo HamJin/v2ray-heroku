f575f7137a470cc6c3238e1c8f383f887e47241a
railjty <335908093@qq.com>
2020-11-05 00:29:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ chmod +x /v2raybin/v2ray
 rm -rf v2ray.zip 
 chgrp -R 0 /v2raybin 
 chmod -R g+rwX /v2raybin 
+cd v2raybin
 curl -L -H "Cache-Control: no-cache" https://raw.githubusercontent.com/railjty/v2ray-heroku/master/config1.json > ./config1.json ;
 sed "s/66666/$PORT/" config1.json > config.json ;
 ./v2ray
