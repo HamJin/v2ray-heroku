ca91dacb7e5809f0c0fd032fa724e71790116029
railjty <335908093@qq.com>
2020-11-05 00:20:17 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 mkdir -m 777 /v2raybin
 cd /v2raybin
-curl -L -H "Cache-Control: no-cache" -o v2ray.zip https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip 
+curl -L -H "Cache-Control: no-cache" -o v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip 
 unzip v2ray.zip 
 unzip v2ray.zip 
 chmod +x /v2raybin/v2ray 
